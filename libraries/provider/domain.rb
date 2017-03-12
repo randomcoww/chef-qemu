1d706bf392c828efa37965530f22557a4933a3e9
randomcoww <etherblimp@gmail.com>
2017-03-11 16:53:10 -0800

    fix

--- libraries/provider/domain.rb
+++ libraries/provider/domain.rb
@@ -1,13 +1,11 @@
 class ChefQemu
   class Provider
     class Domain < Chef::Provider
-      include QemuHelper
-
       provides :qemu_domain, os: "linux"
 
       def load_current_resource
         @current_resource = ChefQemu::Resource::Domain.new(new_resource.name)
-        current_resource.domain(LibvirtDomain.get_current_from_xml(current_resource.xml))
+        current_resource.domain(QemuHelper::LibvirtDomain.get_current_from_xml(current_resource.xml))
 
         current_resource
       end
@@ -41,7 +39,7 @@ class ChefQemu
       def action_define
         if current_resource.domain.nil?
           converge_by("Define domain: #{new_resource}") do
-            LibvirtDomain.define_from_xml(new_resource.xml)
+            QemuHelper::LibvirtDomain.define_from_xml(new_resource.xml)
           end
         end
       end
@@ -49,7 +47,7 @@ class ChefQemu
       def action_start
         if current_resource.domain.nil? || !current_resource.domain.active?
           converge_by("Start domain: #{new_resource}") do
-            domain = LibvirtDomain.get_or_define_from_xml(new_resource.xml)
+            domain = QemuHelper::LibvirtDomain.get_or_define_from_xml(new_resource.xml)
             domain.start(new_resource.timeout)
           end
         end
@@ -58,7 +56,7 @@ class ChefQemu
       def action_autostart
         if current_resource.domain.nil? || !current_resource.domain.autostart?
           converge_by("Set domain autostart: #{new_resource}") do
-            domain = LibvirtDomain.get_or_define_from_xml(new_resource.xml)
+            domain = QemuHelper::LibvirtDomain.get_or_define_from_xml(new_resource.xml)
             domain.set_autostart(true)
           end
         end
