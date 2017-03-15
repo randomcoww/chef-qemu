baab46d2e29301d8ceca39ee0ff39e0eb5ef7a0b
randomcoww <etherblimp@gmail.com>
2017-03-14 23:03:06 -0700

    add parsing for systemd unit hash

--- libraries/_cloud_init.rb
+++ libraries/_cloud_init.rb
@@ -1,4 +1,20 @@
 module CloudInit
+  require 'iniparse'
+
   USER_DATA ||= 'user-data'
   META_DATA ||= 'meta-data'
+
+  ## use same method as chef systemd-unit resource to parse this content
+  # https://github.com/chef/chef/blob/master/lib/chef/resource/systemd_unit.rb#L48-L56
+  def to_ini(content)
+    IniParse.gen do |doc|
+      content.each_pair do |sect, opts|
+        doc.section(sect) do |section|
+          opts.each_pair do |opt, val|
+            section.option(opt, val)
+          end
+        end
+      end
+    end.to_s
+  end
 end
--- libraries/resource/cloud_config.rb
+++ libraries/resource/cloud_config.rb
@@ -1,6 +1,8 @@
 class ChefQemu
   class Resource
     class CloudConfig < Chef::Resource
+      require CloudInit
+
       resource_name :qemu_cloud_config
 
       default_action :create
@@ -10,11 +12,11 @@ class ChefQemu
       property :hostname, String
       property :config, Hash
       property :path, String
+      ## built in parsing for systemd hash
+      ## format: file_path => systemd_unit as hash
+      property :systemd_hash, Hash, default: {}
 
-      property :user_data_content, String, default: lazy {
-        ['#cloud-config', config.to_hash.to_yaml].join($/)
-      }
-
+      property :user_data_content, String, default: lazy { to_conf }
       property :meta_data_content, String, default: lazy {
         {
           'instance-id' => "iid-#{hostname}",
@@ -30,6 +32,21 @@ class ChefQemu
       def meta_data_path
         ::File.join(path, CloudInit::META_DATA)
       end
+    end
+
+    private
+
+    def to_conf
+      content = config.to_hash.dup
+      content['write_files'] ||= []
+
+      systemd_hash.each do |path, unit|
+        content['write_files'] << {
+          "path" => path,
+          "content" => to_ini(unit)
+        }
+      end
+      ['#cloud-config', content.to_yaml].join($/)
     end
   end
 end
