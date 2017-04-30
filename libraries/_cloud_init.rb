96acbceff66a1a1fde780ee7ee21affd6fdb02fc
randomcoww <etherblimp@gmail.com>
2017-04-30 11:41:15 -0700

    modify to_ini to allow duplicate sections

--- libraries/_cloud_init.rb
+++ libraries/_cloud_init.rb
@@ -4,17 +4,47 @@ module CloudInit
   USER_DATA ||= 'user-data'
   META_DATA ||= 'meta-data'
 
-  ## use same method as chef systemd-unit resource to parse this content
-  # https://github.com/chef/chef/blob/master/lib/chef/resource/systemd_unit.rb#L48-L56
-  def to_ini(content)
-    IniParse.gen do |doc|
-      content.each_pair do |sect, opts|
-        doc.section(sect) do |section|
-          opts.each_pair do |opt, val|
-            section.option(opt, val)
-          end
+  def to_ini(c)
+    ini_sections(c).join($/)
+  end
+
+
+  private
+
+  def ini_sections(c, res=[])
+    c.each_pair do |k, v|
+      case v
+      when Array
+        v.each do |j|
+          ini_sections({k => j}, res)
         end
+
+      when Hash
+        res << "[#{k}]"
+        ini_options(v, res)
+        res << ""
       end
-    end.to_s
+    end
+    res
+  end
+
+  def ini_options(c, res=[])
+    c.each_pair do |k, v|
+      case v
+      when Array
+        v.each do |j|
+          ini_options({k => j}, res)
+        end
+
+      when Hash
+        next
+
+      when NilClass
+        res << k
+
+      else
+        res << [k, v].join('=')
+      end
+    end
   end
 end
